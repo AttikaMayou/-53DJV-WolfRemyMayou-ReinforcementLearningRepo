666dab4c3aad6bce515507ee1d0aa01426c56e5d
Benoît <benoitremy74@gmail.com>
2021-03-10 14:59:54 +0100

    Add Function For Check Collision

--- [53DJV-WolfMayou]ReinforcementLearning/Assets/Scripts/SokobanController.cs
+++ [53DJV-WolfMayou]ReinforcementLearning/Assets/Scripts/SokobanController.cs
@@ -39,38 +39,6 @@ public class SokobanController : MonoBehaviour
         grid.Sokoban();
     }
 
-    public void UpIntent()
-    {
-        if (_player.transform.position.z < grid.gridHeight - 1)
-        {
-            _player.transform.position += Vector3.forward;
-        }
-    }
-
-    public void DownIntent()
-    {
-        if (_player.transform.position.z > 0)
-        {
-            _player.transform.position -= Vector3.forward;
-        }
-    }
-
-    public void LeftIntent()
-    {
-        if (_player.transform.position.x > 0)
-        {
-            _player.transform.position += Vector3.left;
-        }
-    }
-
-    public void RightIntent()
-    {
-        if (_player.transform.position.x < grid.gridWidth - 1)
-        {
-            _player.transform.position -= Vector3.left;
-        }
-    }
-
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Z))
@@ -88,6 +56,60 @@ public class SokobanController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             RightIntent();
+        }
+    }
+
+    // Check Collision With Player
+    private bool checkCollision(Vector3 targetPosition)
+    {
+        int x = (int)targetPosition.x;
+        int z = (int)targetPosition.z;
+
+        // If Wall
+        if (grid.grid[z][x].cellSokobanType == Cell.CellSokobanType.Wall)
+        {
+            Debug.Log("Un mur bloque le chemin pour avancer.");
+            return false;
+        }
+
+        // If Crate
+
+        return true;
+    }
+
+    public void UpIntent()
+    {
+        Vector3 targetPosition = _player.transform.position + Vector3.forward;
+        if (_player.transform.position.z < grid.gridHeight - 1 && checkCollision(targetPosition))
+        {
+            _player.transform.position += Vector3.forward;
+        }
+    }
+
+    public void DownIntent()
+    {
+        Vector3 targetPosition = _player.transform.position - Vector3.forward;
+        if (_player.transform.position.z > 0 && checkCollision(targetPosition))
+        {
+            _player.transform.position -= Vector3.forward;
+        }
+    }
+
+    public void LeftIntent()
+    {
+        Vector3 targetPosition = _player.transform.position + Vector3.left;
+        if (_player.transform.position.x > 0 && checkCollision(targetPosition))
+        {
+            _player.transform.position += Vector3.left;
+        }
+    }
+
+    public void RightIntent()
+    {
+        Vector3 targetPosition = _player.transform.position - Vector3.left;
+        if (_player.transform.position.x < grid.gridWidth - 1 && checkCollision(targetPosition))
+        {
+            _player.transform.position -= Vector3.left;
         }
     }
 }
