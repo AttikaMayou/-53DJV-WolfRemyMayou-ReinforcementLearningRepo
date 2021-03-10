13a42e840d2f87720d3ed207503a4f4977a0dacc
Benoît <benoitremy74@gmail.com>
2021-03-10 12:01:02 +0100

    Start SokobanController

--- [53DJV-WolfMayou]ReinforcementLearning/Assets/Scripts/SokobanController.cs
+++ [53DJV-WolfMayou]ReinforcementLearning/Assets/Scripts/SokobanController.cs
@@ -6,8 +6,80 @@ public class SokobanController : MonoBehaviour
 {
     [SerializeField] public Grid grid;
 
+    [SerializeField] public GameObject playerPrefab;
+    public GameObject _player;
+
+    // Number of strokes
+    private int strokeNumber;
+    private bool gameIsFinished = false;
+
+    public enum SokobanIntent
+    {
+        Down,
+        Up,
+        Left,
+        Right
+    }
+
     public void InitSokobanGrid()
     {
+        // Set Sokoban Grid Size
+        grid.gridWidth = 4;
+        grid.gridHeight = 4;
         grid.Sokoban();
+        _player = Instantiate(playerPrefab, grid.startPos + new Vector3(0, 0.5f, 0), Quaternion.identity);
+        _player.transform.SetParent(this.transform);
+    }
+
+    public void UpIntent()
+    {
+        if (_player.transform.position.z < grid.gridHeight - 1)
+        {
+            _player.transform.position += Vector3.forward;
+        }
+    }
+
+    public void DownIntent()
+    {
+        if (_player.transform.position.z > 0)
+        {
+            _player.transform.position -= Vector3.forward;
+        }
+    }
+
+    public void LeftIntent()
+    {
+        if (_player.transform.position.x > 0)
+        {
+            _player.transform.position += Vector3.left;
+        }
+    }
+
+    public void RightIntent()
+    {
+        if (_player.transform.position.x < grid.gridWidth - 1)
+        {
+            _player.transform.position -= Vector3.left;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            UpIntent();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            DownIntent();
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            LeftIntent();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            RightIntent();
+        }
     }
 }
